29429bb0f79a776383fa91b45f9d8ac679c2c6ec
Momanyi Poet <momanyiturner@gmail.com>
2024-02-27 21:41:15 +0300

    added condolence funcionality

---
+++ frontend/src/layouts/Condolence.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect  } from 'react';
+
+const Condolence = () => {
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [displayCount, setDisplayCount] = useState(4); // Initial number of articles to display
+
+    useEffect(() => {
+        // Fetch message
+        fetch('https://backend-momanyi.onrender.com/farewell/api/condolence/')
+            .then(response => response.json())
+            .then(data => {
+                setMessages(data);
+                setLoading(false); // Set loading to false when messages are fetched
+            })
+            .catch(error => {
+                console.error('Error fetching messages:', error);
+                setLoading(false); // Set loading to false in case of error
+            });
+    }, []);
+
+    const handleLoadMore = () => {
+        // Increase the display count to load more comments
+        setDisplayCount(displayCount + 4);
+    };
+
+  return (
+    <div className="lg:col-span-3">
+        <div className="grid md:grid-cols-2 gap-4">
+            {loading ? (
+                <div className="flex justify-center items-center py-4">
+                    <span className="loading loading-dots loading-lg text-center text-primary"></span>
+                </div>
+            ):(
+                <>
+                    {messages.slice().reverse().slice(0, displayCount).map((message) => (
+                        <div className="bg-secondary text-primary py-4 px-8 rounded-3xl " key={message.id}>
+                            <div className="flex flex-row justify-between pt-8">
+                                <h6 className="font-semibold">{message.full_name}</h6>
+                                <span className="text-5xl">&quot;</span>
+                            </div>
+                            <div className="flex flex-col space-y-8">
+                                <p className="text-sm italic">{message.message}</p>
+                                <span className="text-5xl">&quot;</span>
+                            </div>
+                        </div>
+                    ))}
+                </>
+            )}
+
+        </div>
+
+        <div className="text-white font-semibold text-sm text-center py-4">
+            {displayCount < messages.length && (
+                <button onClick={handleLoadMore} className="bg-primary focus:outline-none focus:ring focus:ring-cyan-300 py-3 px-6 rounded-3xl transition duration-200 transform hover:scale-110">Load More</button>
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default Condolence
--- frontend/src/layouts/Messages.jsx
+++ frontend/src/layouts/Messages.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Condolence from './Condolence'
 
 const Messages = () => {
+
+    const [fullName, setFullName] = useState('');
+    const [message, setMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setLoading(true); // Set loading to true when the form is submitted
+        const fullNameValue = event.target.elements.fullName.value;
+        const messageValue = event.target.elements.message.value;
+
+        fetch('https://backend-momanyi.onrender.com/farewell/api/condolence/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                full_name: fullNameValue,
+                message: messageValue,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.id) {
+                    // Assuming the presence of an 'id' property indicates a successful submission
+                    setSuccessMessage('Message submitted successfully');
+                    setFullName('');
+                    setMessage('');
+                    setTimeout(() => {
+                        setSuccessMessage('');
+                    }, 4000); // 4 seconds
+                } else {
+                    // Handle error, e.g., show an error message
+                    setSuccessMessage('Failed to submit message');
+                }
+            })
+            .catch(error => {
+                console.error('Error submitting comment:', error);
+                setSuccessMessage('An error occurred while submitting the message');
+                setFullName('');
+                setMessage('');
+            })
+            .finally(() => {
+                setLoading(false); // Set loading back to false after the submission process is complete
+            });
+    };
+
   return (
     <section className="py-12 scroll-m-12" id="messages">
+        {successMessage &&
+            <div className="toast toast-bottom toast-end text-sm font-semibold italic z-50">
+                <div className="alert alert-info">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>You have a new Notification</span>
+                </div>
+                <div className="alert alert-success text-whity">
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>{successMessage}</span>
+                </div>
+            </div>
+        }
+
         <div className="text-center text-primary text-2xl font-semibold py-4">
             <h3>Messages of Condolence</h3>
         </div>
@@ -15,22 +78,23 @@ const Messages = () => {
                     <h3 className="text-2xl">WITH A MESSAGE</h3>
                 </div>
 
-                <form method="post">
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className="form-control w-full py-2">
                             <div className="label">
                                 <span className="label-text text-white">Name</span>
                             </div>
-                            <input type="text" placeholder="You can call me..." className="input input-bordered w-full italic shadow-md text-primary text-sm bg-white" required />
+                            <input type="text" name="fullName" placeholder="You can call me..." className="input input-bordered w-full italic shadow-md text-primary text-sm bg-white" required value={fullName} onChange={(e) => setFullName(e.target.value)} />
                         </label>
 
                         <label className="form-control w-full py-2">
                             <div className="label">
                                 <span className="label-text text-white">Message</span>
                             </div>
-                            <textarea className="textarea textarea-bordered h-24 italic shadow-md text-primary text-sm bg-white" placeholder="I would like to say..." required></textarea>
+                            <textarea name="message" className="textarea textarea-bordered h-24 italic shadow-md text-primary text-sm bg-white" placeholder="I would like to say..." required value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
                         </label>
 
+                        {loading && <p className='text-xs italic '>Sending Message...</p>}
                         <label className="form-control w-full py-6">
                             <div className="text-primary font-semibold text-end">
                                 <button value="submit" type="submit" className="bg-white focus:outline-none focus:ring focus:ring-cyan-300 py-2 px-4 rounded-3xl transition duration-200 transform hover:scale-110">Send Message</button>
@@ -42,58 +106,7 @@ const Messages = () => {
             </div>
 
             {/* Right */}
-            <div className="lg:col-span-3">
-                <div className="grid md:grid-cols-2 gap-4">
-                    <div className="bg-secondary text-primary py-4 px-8 rounded-3xl ">
-                        <div className="flex flex-row justify-between pt-8">
-                            <h6 className="font-semibold">Mary Moraa</h6>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                        <div className="flex flex-col space-y-8">
-                            <p className="text-sm italic">The sensitivity and professionalism shown by Poet Funeral Ceremony made a difficult time a little easier to bear</p>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                    </div>
-
-                    <div className="bg-secondary text-primary py-4 px-8 rounded-3xl ">
-                        <div className="flex flex-row justify-between pt-8">
-                            <h6 className="font-semibold">Mary Moraa</h6>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                        <div className="flex flex-col space-y-8">
-                            <p className="text-sm italic">The sensitivity and professionalism shown by Poet Funeral Ceremony made a difficult time a little easier to bear</p>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                    </div>
-
-                    <div className="bg-secondary text-primary py-4 px-8 rounded-3xl ">
-                        <div className="flex flex-row justify-between pt-8">
-                            <h6 className="font-semibold">Mary Moraa</h6>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                        <div className="flex flex-col space-y-8">
-                            <p className="text-sm italic">The sensitivity and professionalism shown by Poet Funeral Ceremony made a difficult time a little easier to bear</p>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                    </div>
-
-                    <div className="bg-secondary text-primary py-4 px-8 rounded-3xl ">
-                        <div className="flex flex-row justify-between pt-8">
-                            <h6 className="font-semibold">Mary Moraa</h6>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                        <div className="flex flex-col space-y-8">
-                            <p className="text-sm italic">The sensitivity and professionalism shown by Poet Funeral Ceremony made a difficult time a little easier to bear</p>
-                            <span className="text-5xl">&quot;</span>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="text-white font-semibold text-sm text-center py-4">
-                    <button value="submit" type="submit" className="bg-primary focus:outline-none focus:ring focus:ring-cyan-300 py-3 px-6 rounded-3xl transition duration-200 transform hover:scale-110">Load More</button>
-                </div>
-            </div>
+            < Condolence />
         </div>
     </section>
   )
