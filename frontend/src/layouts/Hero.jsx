4789d3f1864185e0ec73ac7cd0ecb137db71f3dd
Momanyi Poet <momanyiturner@gmail.com>
2024-03-01 18:18:01 +0300

    modified hero text

--- frontend/src/layouts/Hero.jsx
+++ frontend/src/layouts/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="mt-8 grid lg:grid-cols-5 gap-8 text-white font-semibold">
             <div className="lg:col-span-2 flex flex-col justify-center items-center space-y-2">
                 <div>
-                    <h6 className="text-xs">HONORING THE LIFE OF</h6>
+                    <h6 className="text-sm md:text-base">HONORING THE LIFE OF</h6>
                     <h1 className="text-4xl lg:text-5xl">YUNUKE</h1>
                     <h1 className="text-4xl lg:text-5xl">BOSIBORI</h1>
                     <h1 className="text-4xl lg:text-5xl">NYAMBANE</h1>
