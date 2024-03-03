f91cfa4dbb51ab21edf5b7bb78b71da29dfb6b59
Momanyi Poet <momanyiturner@gmail.com>
2024-03-03 03:46:28 +0300

    added analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import React from "react";
 import Landing from './pages/Landing';
 import Welcome from './pages/Welcome';
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
 function App() {
 
   return (
