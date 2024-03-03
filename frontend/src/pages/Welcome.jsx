5fcc17e3f6a4683a241a963a2bbe8d7ca813bc40
Momanyi Poet <momanyiturner@gmail.com>
2024-03-03 03:39:44 +0300

    updated google fonts and fontawsome icons

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import waveTop from '../assets/images/dark-wave-haikei-up.svg';
-import waveDown from '../assets/images/dark-wave-haikei-down.svg';
-
 const Welcome = () => {
 
     // Changing code dynamically
@@ -29,18 +26,11 @@ const Welcome = () => {
 
   return (
     <div id="" className="fixed top-0 left-0 w-full min-h-screen bg-darky flex flex-col items-center justify-center z-50">
-        {/* <div className="-mt-5 md:-mt-20">
-            <img src={waveTop} alt="Wave svg" title="Wave SVG" />
-        </div> */}
 
         <div className="font-normal text-white flex items-center justify-center">
-            <span className="text-2xl mr-2"><i className="fa-solid fa-location-dot"></i> </span>
             <h1 className="text-4xl">{greetings[currentGreetingIndex]}</h1>
         </div>
-        
-        {/* <div className="-mb-5 md:-mb-20">
-            <img src={waveDown} alt="Wave svg" title="Wave SVG" />
-        </div> */}
+
     </div>
   )
 }
